45d23aabb675ed22ff20465255380a1c17afeae2
Arseniy Bulaev <seni190999@mail.ru>
2022-04-18 22:09:54 +0500

    Доработка метода поиска ближайшего к треугольника

--- Triangulation/Simple Iterative Method/triangulation.py
+++ Triangulation/Simple Iterative Method/triangulation.py
@@ -30,26 +30,32 @@ class Triangle(object):
         return other_nodes
 
 
-def find_nearest_triangle(came_from, new_node):
+def find_nearest_triangle(current_triangle, new_node, came_from=None):
     def signed_area(a, b, c):
         return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x)
 
-    for node, node_index in enumerate(came_from.nodes):
+    for node_index, node in enumerate(current_triangle.nodes):
         # Ребро, которое лежит напротив данного узла
-        edge = came_from.get_other_nodes(node)
+        edge = current_triangle.get_other_nodes(node)
         # Узлы ребра, которое лежит напротив проверяемого узла
         node_1, node_2 = edge
         # Проверяем, что текущий и новый узел
         # лежат по разную сторону от данного ребра
         if signed_area(node_1, node_2, node) * signed_area(node_1, node_2, new_node) <= 0:
             # Получаем треугольник, которые лежит напротив данного узла
-            next_triangle = came_from.triangles[node_index]
+            next_triangle = current_triangle.triangles[node_index]
+
+            # Предотвращаем зацикливание
+            if came_from is not None:
+                if next_triangle is came_from:
+                    return current_triangle
+
             if next_triangle is None:
                 # Если такого нет, то ближайшим будет тот, который мы рассматриваем
-                return came_from
+                return current_triangle
             else:
                 # Иначе запускаем рекурсивно для противолежащего треугольника
-                return find_nearest_triangle(next_triangle, new_node)
+                return find_nearest_triangle(next_triangle, new_node, came_from=current_triangle)
 
     # Если дошли до сюда, то новая точка лежит в текущем треугольнике
-    return came_from
+    return current_triangle
