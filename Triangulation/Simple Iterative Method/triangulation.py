cdab3ede359b5397ce18808f93c60bee3a5cb0ff
Arseniy Bulaev <seni190999@mail.ru>
2022-04-20 20:56:58 +0500

    Каркас основного метода

--- Triangulation/Simple Iterative Method/triangulation.py
+++ Triangulation/Simple Iterative Method/triangulation.py
@@ -1,54 +1,27 @@
-class Node(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __repr__(self):
-        return "({0},{1})".format(self.x, self.y)
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
+from triangulation_objects import Node, Triangle
 
 
-class Triangle(object):
-    def __init__(self, nodes, triangles, number=None):
-        self.number = number
-        self.nodes = nodes
-        self.triangles = triangles
-
-    def __repr__(self):
-        return "{0}".format(self.nodes)
-
-    def center(self):
-        x = (self.nodes[0].x + self.nodes[1].x + self.nodes[2].x) / 3
-        y = (self.nodes[0].y + self.nodes[1].y + self.nodes[2].y) / 3
-        return Node(x, y)
-
-    def get_other_nodes(self, node):
-        other_nodes = self.nodes.copy()
-        other_nodes.remove(node)
-        return other_nodes
+def signed_area(a, b, c):
+    return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x)
 
 
 def find_nearest_triangle(current_triangle, new_node, came_from=None):
-    def signed_area(a, b, c):
-        return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x)
 
-    for node_index, node in enumerate(current_triangle.nodes):
+    for current_node_index, current_node in enumerate(current_triangle.nodes):
         # Ребро, которое лежит напротив данного узла
-        edge = current_triangle.get_other_nodes(node)
-        # Узлы ребра, которое лежит напротив проверяемого узла
-        node_1, node_2 = edge
+        opposite_edge = current_triangle.get_other_nodes(current_node)
+
         # Проверяем, что текущий и новый узел
         # лежат по разную сторону от данного ребра
-        if signed_area(node_1, node_2, node) * signed_area(node_1, node_2, new_node) <= 0:
-            # Получаем треугольник, которые лежит напротив данного узла
-            next_triangle = current_triangle.triangles[node_index]
+        if signed_area(opposite_edge[0], opposite_edge[1], current_node)\
+                * signed_area(opposite_edge[0], opposite_edge[1], new_node) <= 0:
 
-            # Предотвращаем зацикливание
-            if came_from is not None:
-                if next_triangle is came_from:
-                    return current_triangle
+            # Получаем треугольник, которые лежит напротив данного узла
+            next_triangle = current_triangle.triangles[current_node_index]
+
+            # Если следующий треугольник тот, из которого мы пришли, то возвращаем текщий треугольник
+            if came_from is not None and next_triangle is came_from:
+                return current_triangle
 
             if next_triangle is None:
                 # Если такого нет, то ближайшим будет тот, который мы рассматриваем
@@ -59,3 +32,45 @@ def find_nearest_triangle(current_triangle, new_node, came_from=None):
 
     # Если дошли до сюда, то новая точка лежит в текущем треугольнике
     return current_triangle
+
+
+def get_node_position_type(triangle, node):
+    return ''
+
+
+def position_based_action(node_position_type, triangulation, nearest_triangle):
+    return []
+
+
+
+def delaunay_check(triangle):
+    return False, None
+
+
+def flip(triangle, unsatisfying_node):
+    pass
+
+
+
+def simple_iterative_algorithm(nodes):
+    # Строим первый треугольник
+    triangulation = Triangle([nodes[0], nodes[1], nodes[2]])
+
+    for node in nodes[3:]:
+        # Ближайший к данному узлу треугольник
+        nearest_triangle = find_nearest_triangle(triangulation, node, None)
+
+        # Тип Позиции нового узла относительно найденного треугольника
+        # 'edge' - на ребре
+        # 'inside' - внутри
+        # 'outside'- снаружи
+        node_position_type = get_node_position_type(nearest_triangle, node)
+
+        # Стрим новые треугольники
+        triangles_to_check = position_based_action(node_position_type, triangulation, nearest_triangle)
+
+        # Проверяем построенные треугольники
+        for triangle in triangles_to_check:
+            delaunay_is_done, unsatisfying_node = delaunay_check(triangle)
+            if not delaunay_is_done:
+                flip(triangle, unsatisfying_node)
