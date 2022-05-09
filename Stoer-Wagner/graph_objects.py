308d7ba901adc4e7a189ed10280f73a7ddef838b
Arseny <seni190999@mail.ru>
2022-05-09 19:22:54 +0500

    Добавил методы, необходимые для инициализации графа

--- Stoer-Wagner/graph_objects.py
+++ Stoer-Wagner/graph_objects.py
@@ -1,7 +1,14 @@
 class Node(object):
-    def __init__(self, label, nodes):
+    def __init__(self, label):
+        # Метка вершины
         self.label = label
-        self.weighted_adjacent_nodes = nodes
+        # Смежные вершины
+        self.weighted_adjacent_nodes = []
+    
+    def add_adjacent_node(self, adjacent_node, weight):  
+        # Добавление смежной вершины вместе с её стоимостью
+        self.weighted_adjacent_nodes.append((adjacent_node, weight))
+
     
     def __repr__(self):
         return self.label
@@ -9,3 +16,12 @@ class Node(object):
 class Graph(object):
     def __init__(self, nodes):
         self.nodes = nodes
+
+    def connect_nodes(self, first_node, second_node, weight):
+        error_message = "узел с меткой '{0}' не принадлежит графу"
+
+        assert first_node in self.nodes, error_message.format(first_node.label)
+        assert second_node in self.nodes, error_message.format(first_node.label)
+
+        first_node.add_adjacent_node(second_node, weight)
+        second_node.add_adjacent_node(first_node, weight)
